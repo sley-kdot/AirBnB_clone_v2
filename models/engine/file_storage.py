c118c12883d3927ad0f2313b73967f4bfe6b0526
MungaSoftwiz <mwachilumobm@gmail.com>
2024-01-16 22:56:36 +0300

    Add method to delete obj from __objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,9 @@ class FileStorage:
                 self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Delete obj from __objects """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
