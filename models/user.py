3cfe9b07e85e9a14e77aedc9ee0681826e0f7f6c
MungaSoftwiz <mwachilumobm@gmail.com>
2024-01-18 02:03:09 +0300

    Update user class to include db columns

--- models/user.py
+++ models/user.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if storage_type == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
