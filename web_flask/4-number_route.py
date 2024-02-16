ed80ea1a2b2f9f384608c994971961db89914aa0
MungaSoftwiz <mwachilumobm@gmail.com>
2024-02-17 01:27:24 +0300

    Fix print in route to accept dynamic values

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -47,7 +47,7 @@ def hbnb_python(text="is cool"):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """ Display 'n is a number' only if n is an integer """
-    return "n is a number"
+    return "{} is a number".format(n)
 
 
 if __name__ == '__main__':
