374adf6c4dcc648e429faeb2163b05e9eb76672b
MungaSoftwiz <mwachilumobm@gmail.com>
2024-02-17 02:15:06 +0300

    Add method for desirializing JSON to file objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,7 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
+
+    def close(self):
+        """ Calls reload method for desirializing JSON file to objects"""
+        self.reload()
