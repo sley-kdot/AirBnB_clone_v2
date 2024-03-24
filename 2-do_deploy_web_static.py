336b85a1820db7ebe80254ebb08340465ecc0b1c
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-03-24 02:18:13 -0700

    fix: 404 error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,7 +22,7 @@ def do_deploy(archive_path):
         # upload the .tgz archive to /tmp
         put(archive_path, "/tmp")
         file_path = archive_path[9:34]
-        
+
         # unpack the archive file
         file_name = archive_path[9:]
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
@@ -31,6 +31,9 @@ def do_deploy(archive_path):
         # del the archive file from tmp dir
         run("rm /tmp/{}".format(file_name))
         run("rm -rf /data/web_static/current")
+
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(file_path, file_path))
 
         # create a new sym-link ponting to the new version
         run("ln -s /data/web_static/releases/{} /data/web_static/current"
