1fd347f9bf13ed97650f74816eb57797b924c74f
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-03-24 02:01:29 -0700

    fix: symbolic link

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,29 +21,20 @@ def do_deploy(archive_path):
     try:
         # upload the .tgz archive to /tmp
         put(archive_path, "/tmp")
-
-        # # create a dir if not exist on the remote host
         file_path = archive_path[9:34]
-        run("mkdir -p /data/web_static/releases/{}".format(file_path))
-
+        
         # unpack the archive file
-        filename = archive_path[9:]
+        file_name = archive_path[9:]
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-            .format(filename, file_path))
+            .format(file_name, file_path))
 
         # del the archive file from tmp dir
         run("rm /tmp/{}".format(file_name))
-
-        # move and del unpacked dir
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}".format(file_name, file_name))
-
-        run("rm -rf /data/web_static/releases/{}/web_static".format(file_name))
         run("rm -rf /data/web_static/current")
 
         # create a new sym-link ponting to the new version
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(file_name))
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(file_path))
 
         print("New version deployed")
         return True
